40399bcdada2603096f73133d85faf44f85926c8
Ivan Kolodiy <pothuyjohn@gmail.com>
2022-03-12 15:38:25 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,13 +2,7 @@
 
 echo "Installing docker..."
 sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+apt  install docker.io
 
 echo "Running uashield as a daemon with 2000 requests. This value can be changed according to droplet size"
 docker run -d --restart unless-stopped ghcr.io/opengs/uashield:0.0.x 2000 true
