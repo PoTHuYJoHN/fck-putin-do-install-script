ce732c790d2371d2ad98cc2610719eb76054fbfa
Ivan Kolodiy <pothuyjohn@gmail.com>
2022-03-12 16:57:22 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 echo "Installing docker..."
-sudo apt-get update
-apt install docker.io
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce
 
 echo "Running uashield as a daemon with 2000 requests. This value can be changed according to droplet size"
 docker run -d --restart unless-stopped ghcr.io/opengs/uashield:0.0.x 2000 true
