5fcc0584186c3362b000435192144179dd24faf6
Ivan Kolodiy <pothuyjohn@gmail.com>
2022-04-01 17:50:19 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ apt-cache policy docker-ce
 sudo apt -y install docker-ce
 
 echo "Running uashield as a daemon with 2000 requests. This value can be changed according to droplet size"
-docker run -d --restart unless-stopped ghcr.io/opengs/uashield:0.0.x 2000 true
+docker run -d --restart unless-stopped ghcr.io/opengs/uashield:0.0.x 512 true
 
 echo "Creating script to monitor requests via logs..."
 
@@ -21,6 +21,18 @@ while read container
 do
 docker logs -t -f \$container | grep --color -P " 200"
 done < containers.id
+EOT
+
+echo "Creating script to restart docker, pull updates and recreate daemon..."
+
+cat <<EOT >> restart.sh
+#!/bin/bash
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+
+docker ps -a
+docker run -d --restart unless-stopped ghcr.io/opengs/uashield:0.0.x 512 true
+docker ps -a
 EOT
 
 echo "Running script to see requests..."
